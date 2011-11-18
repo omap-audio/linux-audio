4c4a45d4a505076f56a4ee9eeb6290d8fe015f29
Peter Ujfalusi <peter.ujfalusi@ti.com>
2011-11-18 18:25:40 +0200

    OMAP4: devices: Do not create dmic device if the dtb has been provided
    If dtb is provided of will create the needed devices dynamically so there
    is no need to create the platform device for DMIC here.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -344,6 +344,10 @@ static void omap_init_dmic(void)
 	struct omap_hwmod *oh;
 	struct platform_device *pdev;
 
+	/* If dtb is there, the devices will be created dynamically */
+	if (of_have_populated_dt())
+		return;
+
 	oh = omap_hwmod_lookup("dmic");
 	if (!oh) {
 		printk(KERN_ERR "Could not look up mcpdm hw_mod\n");
