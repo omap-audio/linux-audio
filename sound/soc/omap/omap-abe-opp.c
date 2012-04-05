d71f07d48f66e7e51efcd4e6a78f75690e73a847
Sebastien Guiriec <s-guiriec@ti.com>
2012-04-05 18:24:33 +0200

    ASoC: HACK: OMAP5: No OPP selection due to no PM
    Remove OPP selection due to missing PM inside OMAP5 tree.
    By default OPP will be set to 100%.
    
    Signed-off-by: Sebastien Guiriec <s-guiriec@ti.com>

--- sound/soc/omap/omap-abe-opp.c
+++ sound/soc/omap/omap-abe-opp.c
@@ -174,8 +174,9 @@ EXPORT_SYMBOL(omap_abe_opp_free_request);
 
 int abe_opp_set_level(struct omap_abe *abe, int opp)
 {
-	struct omap_abe_pdata *pdata = abe->pdata;
 	int ret = 0;
+#if 0
+	struct omap_abe_pdata *pdata = abe->pdata;
 
 	if (abe->opp.level > opp) {
 		/* Decrease OPP mode - no need of OPP100% */
@@ -259,6 +260,8 @@ err_down_scale:
 
 err_up_scale:
 	dev_err(abe->dev, "opp: failed to scale to OPP%d\n", opp);
+#endif
+	omap_aess_set_opp_processing(abe->aess, ABE_OPP100);
 	return ret;
 }
 
