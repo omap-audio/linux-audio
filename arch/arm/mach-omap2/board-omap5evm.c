f1e7ef40f30ec1a6160479b520107f53c40b50bd
Sebastien Guiriec <s-guiriec@ti.com>
2012-05-10 13:25:48 +0200

    OMAP5: omap5evm: Add support for TCA6424A IO
    TCA6424A IO expender support is needed for HDMI. Add support of the
    device in OMAP5SEVM board.
    
    Signed-off-by: Sebastien Guiriec <s-guiriec@ti.com>

--- arch/arm/mach-omap2/board-omap5evm.c
+++ arch/arm/mach-omap2/board-omap5evm.c
@@ -23,6 +23,8 @@
 #include <linux/mfd/palmas.h>
 #endif
 
+#include <linux/i2c/pca953x.h>
+
 #include <linux/i2c/twl.h>
 #include <linux/mfd/twl6040.h>
 #include <linux/platform_data/omap-abe-twl6040.h>
@@ -590,6 +592,21 @@ static struct i2c_board_info __initdata omap5evm_i2c_1_boardinfo[] = {
 	},
 };
 
+/*
+ * I2C GPIO Expander - TCA6424
+ */
+
+/* Mounted on Base-Board */
+static struct pca953x_platform_data omap5evm_gpio_expander_info = {
+	.gpio_base	= OMAP_MAX_GPIO_LINES,
+};
+
+static struct i2c_board_info __initdata omap5evm_i2c_5_boardinfo[] = {
+	{
+		I2C_BOARD_INFO("tca6424", 0x22),
+		.platform_data = &omap5evm_gpio_expander_info,
+	},
+};
 
 static int __init omap_5430evm_i2c_init(void)
 {
@@ -599,7 +616,8 @@ static int __init omap_5430evm_i2c_init(void)
 	omap_register_i2c_bus(2, 400, NULL, 0);
 	omap_register_i2c_bus(3, 400, NULL, 0);
 	omap_register_i2c_bus(4, 400, NULL, 0);
-	omap_register_i2c_bus(5, 400, NULL, 0);
+	omap_register_i2c_bus(5, 400, omap5evm_i2c_5_boardinfo,
+					ARRAY_SIZE(omap5evm_i2c_5_boardinfo));
 
 	return 0;
 }
