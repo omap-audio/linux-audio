6e23a49afd075ccad4554a162c5d970e9c2653b2
Sebastien Guiriec <s-guiriec@ti.com>
2012-05-15 21:53:34 +0200

    ARM: OMAP5: board-omap5evm: Update device for SPDIF
    Update board file for SPDIF output
    
    Signed-off-by: Sebastien Guiriec <s-guiriec@ti.com>
    
    Conflicts:
    
    	arch/arm/mach-omap2/board-omap5evm.c

--- arch/arm/mach-omap2/board-omap5evm.c
+++ arch/arm/mach-omap2/board-omap5evm.c
@@ -514,6 +514,11 @@ static struct platform_device omap5evm_dmic_codec = {
 	.id	= -1,
 };
 
+static struct platform_device omap5evm_spdif_dit_codec = {
+	.name           = "spdif-dit",
+	.id             = -1,
+};
+
 static struct omap_abe_twl6040_data omap5evm_abe_audio_data = {
 	/* Audio out */
 	.has_hs		= ABE_TWL6040_LEFT | ABE_TWL6040_RIGHT,
@@ -546,6 +551,7 @@ static struct platform_device omap5evm_abe_audio = {
 
 static struct platform_device *omap5evm_devices[] __initdata = {
 	&omap5evm_dmic_codec,
+	&omap5evm_spdif_dit_codec,
 	&omap5evm_abe_audio,
 };
 
