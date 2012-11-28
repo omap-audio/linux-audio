4ec37d621ce4f9af1803369e9c5433eef396472b
Sebastien Guiriec <s-guiriec@ti.com>
2012-11-28 15:41:21 +0100

    ASoC: aess: ABE type clean up
    Clean up ABE type information.
    
    Signed-off-by: Sebastien Guiriec <s-guiriec@ti.com>

--- sound/soc/omap/aess/abe_typ.h
+++ sound/soc/omap/aess/abe_typ.h
@@ -1,84 +1,74 @@
 /*
-
-  This file is provided under a dual BSD/GPLv2 license.  When using or
-  redistributing this file, you may do so under either license.
-
-  GPL LICENSE SUMMARY
-
-  Copyright(c) 2010-2011 Texas Instruments Incorporated,
-  All rights reserved.
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of version 2 of the GNU General Public License as
-  published by the Free Software Foundation.
-
-  This program is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
-  The full GNU General Public License is included in this distribution
-  in the file called LICENSE.GPL.
-
-  BSD LICENSE
-
-  Copyright(c) 2010-2011 Texas Instruments Incorporated,
-  All rights reserved.
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions
-  are met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in
-      the documentation and/or other materials provided with the
-      distribution.
-    * Neither the name of Texas Instruments Incorporated nor the names of
-      its contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
+ *
+ * This file is provided under a dual BSD/GPLv2 license.  When using or
+ * redistributing this file, you may do so under either license.
+ *
+ * GPL LICENSE SUMMARY
+ *
+ * Copyright(c) 2010-2012 Texas Instruments Incorporated,
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ * The full GNU General Public License is included in this distribution
+ * in the file called LICENSE.GPL.
+ *
+ * BSD LICENSE
+ *
+ * Copyright(c) 2010-2012 Texas Instruments Incorporated,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Texas Instruments Incorporated nor the names of
+ *     its contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 #include "abe_def.h"
 
 #ifndef _ABE_TYP_H_
 #define _ABE_TYP_H_
+
 /*
  *	BASIC TYPES
  */
-#if 0
-#define MAX_UINT8	((((1L <<  7) - 1) << 1) + 1)
-#define MAX_UINT16	((((1L << 15) - 1) << 1) + 1)
-#define MAX_UINT32	((((1L << 31) - 1) << 1) + 1)
-#define s8 char
-#define u8 unsigned char
-#define s16 short
-#define u16 unsigned short
-#define s32 int
-#define u32 unsigned int
-#else
 #define MAX_UINT8	((((1L <<  7) - 1) << 1) + 1)
 #define MAX_UINT16	((((1L << 15) - 1) << 1) + 1)
 #define MAX_UINT32	((((1L << 31) - 1) << 1) + 1)
 #include <linux/types.h>
-#endif
+
 /* subroutine types */
 typedef void (*abe_subroutine0) (void);
 typedef void (*abe_subroutine1) (u32);
@@ -97,6 +87,7 @@ typedef void (*abe_subroutine4) (u32, u32, u32, u32);
 #define ABE_OPP25 1
 #define ABE_OPP50 2
 #define ABE_OPP100 3
+
 /*
  *	SAMPLES TYPE
  *
@@ -160,110 +151,21 @@ typedef void (*abe_subroutine4) (u32, u32, u32, u32);
 #define TDM_UL_PORT 15
 #define DEBUG_PORT 16
 #define LAST_PORT_ID 17
-/* definitions for the compatibility with HAL05xx */
-#define PDM_DL1_PORT 18
-#define PDM_DL2_PORT 19
-#define PDM_DL3_PORT 20
-/* There is only one DMIC port, always used with 6 samples
-	per 96kHz periods   */
-#define DMIC_PORT1 DMIC_PORT
-#define DMIC_PORT2 DMIC_PORT
-#define DMIC_PORT3 DMIC_PORT
 
-/*
- *	Signal processing module names - EQ APS MIX ROUT
- */
-/* equalizer downlink path headset + earphone */
-#define FEAT_EQ1            1
-/* equalizer downlink path integrated handsfree LEFT */
-#define FEAT_EQ2L           (FEAT_EQ1+1)
-/* equalizer downlink path integrated handsfree RIGHT */
-#define FEAT_EQ2R           (FEAT_EQ2L+1)
-/* equalizer downlink path side-tone */
-#define FEAT_EQSDT          (FEAT_EQ2R+1)
-/* equalizer uplink path AMIC */
-#define FEAT_EQAMIC         (FEAT_EQSDT+1)
-/* equalizer uplink path DMIC */
-#define FEAT_EQDMIC         (FEAT_EQAMIC+1)
-/* Acoustic protection for headset */
-#define FEAT_APS1           (FEAT_EQDMIC+1)
-/* acoustic protection high-pass filter for handsfree "Left" */
-#define FEAT_APS2           (FEAT_APS1+1)
-/* acoustic protection high-pass filter for handsfree "Right" */
-#define FEAT_APS3           (FEAT_APS2+1)
-/* asynchronous sample-rate-converter for the downlink voice path */
-#define FEAT_ASRC1          (FEAT_APS3+1)
-/* asynchronous sample-rate-converter for the uplink voice path */
-#define FEAT_ASRC2          (FEAT_ASRC1+1)
-/* asynchronous sample-rate-converter for the multimedia player */
-#define FEAT_ASRC3          (FEAT_ASRC2+1)
-/* asynchronous sample-rate-converter for the echo reference */
-#define FEAT_ASRC4          (FEAT_ASRC3+1)
-/* mixer of the headset and earphone path */
-#define FEAT_MIXDL1         (FEAT_ASRC4+1)
-/* mixer of the hands-free path */
-#define FEAT_MIXDL2         (FEAT_MIXDL1+1)
-/* mixer for audio being sent on the voice_ul path */
-#define FEAT_MIXAUDUL       (FEAT_MIXDL2+1)
-/* mixer for voice communication recording */
-#define FEAT_MIXVXREC       (FEAT_MIXAUDUL+1)
-/* mixer for side-tone */
-#define FEAT_MIXSDT         (FEAT_MIXVXREC+1)
-/* mixer for echo reference */
-#define FEAT_MIXECHO        (FEAT_MIXSDT+1)
-/* router of the uplink path */
-#define FEAT_UPROUTE        (FEAT_MIXECHO+1)
-/* all gains */
-#define FEAT_GAINS          (FEAT_UPROUTE+1)
-#define FEAT_GAINS_DMIC1    (FEAT_GAINS+1)
-#define FEAT_GAINS_DMIC2    (FEAT_GAINS_DMIC1+1)
-#define FEAT_GAINS_DMIC3    (FEAT_GAINS_DMIC2+1)
-#define FEAT_GAINS_AMIC     (FEAT_GAINS_DMIC3+1)
-#define FEAT_GAINS_SPLIT    (FEAT_GAINS_AMIC+1)
-#define FEAT_GAINS_DL1      (FEAT_GAINS_SPLIT+1)
-#define FEAT_GAINS_DL2      (FEAT_GAINS_DL1+1)
-#define FEAT_GAIN_BTUL      (FEAT_GAINS_DL2+1)
-/* sequencing queue of micro tasks */
-#define FEAT_SEQ            (FEAT_GAIN_BTUL+1)
-/* Phoenix control queue through McPDM */
-#define FEAT_CTL            (FEAT_SEQ+1)
-/* list of features of the firmware -------------------------------*/
-#define MAXNBFEATURE    FEAT_CTL
-/* abe_equ_id */
-/* equalizer downlink path headset + earphone */
-#define EQ1 FEAT_EQ1
-/* equalizer downlink path integrated handsfree LEFT */
-#define EQ2L FEAT_EQ2L
-#define EQ2R FEAT_EQ2R
-/* equalizer downlink path side-tone */
-#define EQSDT  FEAT_EQSDT
-#define EQAMIC FEAT_EQAMIC
-#define EQDMIC FEAT_EQDMIC
-/* abe_aps_id */
-/* Acoustic protection for headset */
-#define APS1 FEAT_APS1
-#define APS2L FEAT_APS2
-#define APS2R FEAT_APS3
-/* abe_asrc_id */
-/* asynchronous sample-rate-converter for the downlink voice path */
-#define ASRC1 FEAT_ASRC1
-/* asynchronous sample-rate-converter for the uplink voice path */
-#define ASRC2 FEAT_ASRC2
-/* asynchronous sample-rate-converter for the multimedia player */
-#define ASRC3 FEAT_ASRC3
-/* asynchronous sample-rate-converter for the voice uplink echo_reference */
-#define ASRC4 FEAT_ASRC4
+#define FEAT_MIXDL1         14
+#define FEAT_MIXDL2         15
+#define FEAT_MIXAUDUL       16
+#define FEAT_GAINS          21
+#define FEAT_GAINS_DMIC1    22
+#define FEAT_GAINS_DMIC2    23
+#define FEAT_GAINS_DMIC3    24
+#define FEAT_GAINS_AMIC     25
+#define FEAT_GAIN_BTUL      29
 
 /* abe_mixer_id */
 #define MIXDL1 FEAT_MIXDL1
 #define MIXDL2 FEAT_MIXDL2
-#define MIXSDT FEAT_MIXSDT
-#define MIXECHO FEAT_MIXECHO
 #define MIXAUDUL FEAT_MIXAUDUL
-#define MIXVXREC FEAT_MIXVXREC
-/* abe_router_id */
-/* there is only one router up to now */
-#define UPROUTE  FEAT_UPROUTE
 /*
  *	GAIN IDs
  */
@@ -271,9 +173,6 @@ typedef void (*abe_subroutine4) (u32, u32, u32, u32);
 #define GAINS_DMIC2     FEAT_GAINS_DMIC2
 #define GAINS_DMIC3     FEAT_GAINS_DMIC3
 #define GAINS_AMIC      FEAT_GAINS_AMIC
-#define GAINS_SPLIT     FEAT_GAINS_SPLIT
-#define GAINS_DL1       FEAT_GAINS_DL1
-#define GAINS_DL2       FEAT_GAINS_DL2
 #define GAINS_BTUL      FEAT_GAIN_BTUL
 
 /*
@@ -515,4 +414,4 @@ struct omap_aess_irq_data {
 	unsigned int tag:4;
 };
 
-#endif/* ifndef _ABE_TYP_H_ */
+#endif /* ifndef _ABE_TYP_H_ */
