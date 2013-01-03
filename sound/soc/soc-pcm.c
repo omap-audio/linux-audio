KFire-Android/kernel_omap_otter-common

ASoC: dpcm: Free widget list after playback runtime update
A widget list is allocated and populated while discovering paths connected
to a given FE, this list must be freed upon completion of runtime update
for a given stream direction. Currently, widget list for playback was not
freed.

Change-Id: I59b66f9be0706ab9de4e5e909800f691bae95b35
Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

sound/soc/soc-pcm.c
@@ -1965,6 +1965,8 @@ int soc_dpcm_runtime_update(struct snd_soc_dapm_widget *widget)
 			be_disconnect(fe, SNDRV_PCM_STREAM_PLAYBACK);
 		}
 
+		fe_path_put(&list);
+
 capture:
 		/* skip if FE doesn't have capture capability */
 		if (!fe->cpu_dai->driver->capture.channels_min)

omap-audio/linux-audio

Signed-off-by: Liam Girdwood <lrg@ti.com>

sound/soc/soc-pcm.c
@@ -1923,6 +1923,8 @@ int soc_dpcm_runtime_update(struct snd_soc_dapm_widget *widget)
 			dpcm_be_disconnect(fe, SNDRV_PCM_STREAM_PLAYBACK);
 		}
 
+		dpcm_path_put(&list);
+
 capture:
 		/* skip if FE doesn't have capture capability */
 		if (!fe->cpu_dai->driver->capture.channels_min)