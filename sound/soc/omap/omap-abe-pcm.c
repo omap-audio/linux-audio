de981ef0d98df76c59ed60e4c0c180a877714c4d
Peter Ujfalusi <peter.ujfalusi@ti.com>
2013-03-05 11:32:56 +0100

    ASoC: omap4+: rename omap-abe-mmap.c to omap-abe-pcm.c
    The code inside of the file is the platform part for AESS/ABE so it is
    better to rename the file.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- sound/soc/omap/omap-abe-pcm.c
+++ sound/soc/omap/omap-abe-pcm.c
@@ -1,5 +1,5 @@
 /*
- * omap-abe.c  --  OMAP ALSA SoC DAI driver using Audio Backend
+ * omap-abe-pcm.c  --  OMAP ALSA SoC platform driver using Audio Backend
  *
  * Copyright (C) 2010 Texas Instruments
  *
