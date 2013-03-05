00c58d82a9c19bba3fb1b79663cac7be7406ff01
Peter Ujfalusi <peter.ujfalusi@ti.com>
2013-03-05 10:44:54 +0100

    ASoC: OMAP4+: rename omap-abe-pcm.c to omap-abe-dai.c
    The file contains the DAI part of the AESS/ABE
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- sound/soc/omap/omap-abe-dai.c
+++ sound/soc/omap/omap-abe-dai.c
@@ -1,5 +1,5 @@
 /*
- * omap-abe.c  --  OMAP ALSA SoC DAI driver using Audio Backend
+ * omap-abe-dai.c  --  OMAP ALSA SoC DAI driver using Audio Backend
  *
  * Copyright (C) 2010 Texas Instruments
  *
