kitakar5525/linux-kernel

ASoC: firmware: Add support for dynamic components
Issue: GMINL-3679

Add generic support to create ASoC kcontrols, widgets, graphs and coefficients from
firmware files.  This is useful for many modern audio DSP devices where the firmware
often defines the graph and mixers and allows the vendor firmware file to ship with the
ASoC specific components along with the firmware text.

The generic FW format is very simple and uses a small header to describe each file
section. e.g. a FW file with text and kcontrols would have a header for the text and
another header for the kcontrols section. The sections can be in any order within
the firmware file meaning many different FW section files can be cat'ed together into
a larger file.

This patch creates the generic ASoC firmware core and an associated header API
that can also be used by userspace firmware generation tools. It also defines a
small API that can be used by ASoc component drivers to interrogate and process
the firmware data. Most firmware data is handled by the core, e.g. widgets, kcontrols
and graph, whilst other data is passed directly to the component driver for
enumeration e.g. text.

This patch has working support to load ASoC :-

 o Kcontrols
 o TLV Kcontrols
 o DAPM widgets
 o DAPM Graph
 o Simple Enum Kcontrols with coefficients.
 o Firmware Text
 o Custom vendor blocks

There is a command line firmware file generation tool that is mostly complete at :-

git@gitorious.org:omap-audio/asoc-fw.git

This tool generates the headers and generic components based on the ASoC types above
and it's intended to be upstreamed in to alsa-utils soon. There is example code with
the tool for generating firmware for the OMAP4 ABE.

Change-Id: I506e7c39f4c2a8c0fd2d51a3dae487a7baf588cf
Signed-off-by: Liam Girdwood <lrg@ti.com>

include/sound/soc-fw.h
@@ -1,0 +1,130 @@
+/*
+ * linux/sound/soc-fw.h -- ALSA SoC Firmware Controls and DAPM
+ *
+ * Copyright:	2012 Texas Instruments Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Simple file API to load FW that includes mixers, coefficients, DAPM graphs,
+ * algorithms, equalisers, DAIs, widgets etc.
+ */
+
+#ifndef __LINUX_SND_SOC_FW_H
+#define __LINUX_SND_SOC_FW_H
+
+#include <uapi/sound/asoc.h>
+
+struct firmware;
+
+/*
+ * Kcontrol operations - used to map handlers onto firmware based controls.
+ */
+struct snd_soc_fw_kcontrol_ops {
+	u32 id;
+	int (*get)(struct snd_kcontrol *kcontrol,
+			struct snd_ctl_elem_value *ucontrol);
+	int (*put)(struct snd_kcontrol *kcontrol,
+			struct snd_ctl_elem_value *ucontrol);
+	int (*info)(struct snd_kcontrol *kcontrol,
+		struct snd_ctl_elem_info *uinfo);
+};
+
+/*
+ * Public API - Used by component drivers to load new mixers, DAPM, vendor
+ * specific data.
+ */
+struct snd_soc_fw_codec_ops {
+
+	/* external kcontrol init - can be used to set ext funcs + pdata */
+	int (*control_load)(struct snd_soc_codec *, struct snd_kcontrol_new *);
+
+	/* external widget init - can be used to set ext funcs + pdata */
+	int (*widget_load)(struct snd_soc_codec *, struct snd_soc_dapm_widget *);
+
+	/* callback to handle vendor data */
+	int (*vendor_load)(struct snd_soc_codec *, struct snd_soc_fw_hdr *);
+	int (*vendor_unload)(struct snd_soc_codec *, struct snd_soc_fw_hdr *);
+
+	/* completion - called at completion of firmware loading */
+	void (*complete)(struct snd_soc_codec *);
+
+	/* kcontrols operations */
+	const struct snd_soc_fw_kcontrol_ops *io_ops;
+	int io_ops_count;
+};
+
+struct snd_soc_fw_platform_ops {
+
+	/* external kcontrol init - can be used to set ext funcs + pdata */
+	int (*control_load)(struct snd_soc_platform *, struct snd_kcontrol_new *);
+
+	/* external widget init - can be used to set ext funcs + pdata */
+	int (*widget_load)(struct snd_soc_platform *, struct snd_soc_dapm_widget *);
+
+	/* callback to handle vendor data */
+	int (*vendor_load)(struct snd_soc_platform *, struct snd_soc_fw_hdr *);
+	int (*vendor_unload)(struct snd_soc_platform *, struct snd_soc_fw_hdr *);
+
+	/* completion - called at completion of firmware loading */
+	void (*complete)(struct snd_soc_platform *);
+
+	/* kcontrols operations */
+	const struct snd_soc_fw_kcontrol_ops *io_ops;
+	int io_ops_count;
+};
+
+struct snd_soc_fw_card_ops {
+
+	/* external kcontrol init - can be used to set ext funcs + pdata */
+	int (*control_load)(struct snd_soc_card *, struct snd_kcontrol_new *);
+
+	/* external widget init - can be used to set ext funcs + pdata */
+	int (*widget_load)(struct snd_soc_card *, struct snd_soc_dapm_widget *);
+
+	/* callback to handle vendor data */
+	int (*vendor_load)(struct snd_soc_card *, struct snd_soc_fw_hdr *);
+	int (*vendor_unload)(struct snd_soc_card *, struct snd_soc_fw_hdr *);
+
+	/* completion */
+	void (*complete)(struct snd_soc_card *);
+
+	/* kcontrols operations */
+	const struct snd_soc_fw_kcontrol_ops *io_ops;
+	int io_ops_count;
+};
+
+/* gets a pointer to data from the firmware block header */
+static inline const void *snd_soc_fw_get_data(struct snd_soc_fw_hdr *hdr)
+{
+	const void *ptr = hdr;
+
+	return ptr + sizeof(*hdr);
+}
+
+/* Firmware loading for component drivers */
+int snd_soc_fw_load_card(struct snd_soc_card *card,
+	struct snd_soc_fw_card_ops *ops, const struct firmware *fw,
+	u32 index);
+int snd_soc_fw_load_platform(struct snd_soc_platform *platform,
+	struct snd_soc_fw_platform_ops *ops, const struct firmware *fw,
+	u32 index);
+int snd_soc_fw_load_codec(struct snd_soc_codec *codec,
+	struct snd_soc_fw_codec_ops *ops, const struct firmware *fw,
+	u32 index);
+
+#define SND_SOC_FW_INDEX_ALL	0	/* index that matches all FW objects */
+
+/* Firmware based dynamic widget and assoc kcontrol removal */
+void snd_soc_fw_dcontrols_remove_widgets(struct snd_soc_dapm_context *dapm,
+	u32 index);
+void snd_soc_fw_dcontrols_remove_widget(struct snd_soc_dapm_widget *w);
+
+/* Firmware based dynamic kcontrol removal for components */
+void snd_soc_fw_dcontrols_remove_codec(struct snd_soc_codec *codec, u32 index);
+void snd_soc_fw_dcontrols_remove_platform(struct snd_soc_platform *platform, u32 index);
+void snd_soc_fw_dcontrols_remove_card(struct snd_soc_card *soc_card, u32 index);
+int snd_soc_fw_dcontrols_remove_all(struct snd_soc_card *soc_card, u32 index);
+
+#endif

omap-audio/linux-audio

include/sound/soc-fw.h
@@ -1,0 +1,130 @@
+/*
+ * linux/sound/soc-fw.h -- ALSA SoC Firmware Controls and DAPM
+ *
+ * Copyright:	2012 Texas Instruments Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Simple file API to load FW that includes mixers, coefficients, DAPM graphs,
+ * algorithms, equalisers, DAIs, widgets etc.
+ */
+
+#ifndef __LINUX_SND_SOC_FW_H
+#define __LINUX_SND_SOC_FW_H
+
+#include <uapi/sound/asoc.h>
+
+struct firmware;
+
+/*
+ * Kcontrol operations - used to map handlers onto firmware based controls.
+ */
+struct snd_soc_fw_kcontrol_ops {
+	u32 id;
+	int (*get)(struct snd_kcontrol *kcontrol,
+			struct snd_ctl_elem_value *ucontrol);
+	int (*put)(struct snd_kcontrol *kcontrol,
+			struct snd_ctl_elem_value *ucontrol);
+	int (*info)(struct snd_kcontrol *kcontrol,
+		struct snd_ctl_elem_info *uinfo);
+};
+
+/*
+ * Public API - Used by component drivers to load new mixers, DAPM, vendor
+ * specific data.
+ */
+struct snd_soc_fw_codec_ops {
+
+	/* external kcontrol init - can be used to set ext funcs + pdata */
+	int (*control_load) (struct snd_soc_codec *, struct snd_kcontrol_new *);
+
+	/* external widget init - can be used to set ext funcs + pdata */
+	int (*widget_load) (struct snd_soc_codec *, struct snd_soc_dapm_widget *);
+
+	/* callback to handle vendor data */
+	int (*vendor_load) (struct snd_soc_codec *, struct snd_soc_fw_hdr *);
+	int (*vendor_unload) (struct snd_soc_codec *, struct snd_soc_fw_hdr *);
+
+	/* completion - called at completion of firmware loading */
+	void (*complete) (struct snd_soc_codec *);
+
+	/* kcontrols operations */
+	const struct snd_soc_fw_kcontrol_ops *io_ops;
+	int io_ops_count;
+};
+
+struct snd_soc_fw_platform_ops {
+
+	/* external kcontrol init - can be used to set ext funcs + pdata */
+	int (*control_load) (struct snd_soc_platform *, struct snd_kcontrol_new *);
+
+	/* external widget init - can be used to set ext funcs + pdata */
+	int (*widget_load) (struct snd_soc_platform *, struct snd_soc_dapm_widget *);
+
+	/* callback to handle vendor data */
+	int (*vendor_load) (struct snd_soc_platform *, struct snd_soc_fw_hdr *);
+	int (*vendor_unload) (struct snd_soc_platform *, struct snd_soc_fw_hdr *);
+
+	/* completion - called at completion of firmware loading */
+	void (*complete) (struct snd_soc_platform *);
+
+	/* kcontrols operations */
+	const struct snd_soc_fw_kcontrol_ops *io_ops;
+	int io_ops_count;
+};
+
+struct snd_soc_fw_card_ops {
+
+	/* external kcontrol init - can be used to set ext funcs + pdata */
+	int (*control_load) (struct snd_soc_card *, struct snd_kcontrol_new *);
+
+	/* external widget init - can be used to set ext funcs + pdata */
+	int (*widget_load) (struct snd_soc_card *, struct snd_soc_dapm_widget *);
+
+	/* callback to handle vendor data */
+	int (*vendor_load) (struct snd_soc_card *, struct snd_soc_fw_hdr *);
+	int (*vendor_unload) (struct snd_soc_card *, struct snd_soc_fw_hdr *);
+
+	/* completion */
+	void (*complete) (struct snd_soc_card *);
+
+	/* kcontrols operations */
+	const struct snd_soc_fw_kcontrol_ops *io_ops;
+	int io_ops_count;
+};
+
+/* gets a pointer to data from the firmware block header */
+static inline const void *snd_soc_fw_get_data(struct snd_soc_fw_hdr *hdr)
+{
+	const void *ptr = hdr;
+
+	return ptr + sizeof(*hdr);
+}
+
+/* Firmware loading for component drivers */
+int snd_soc_fw_load_card(struct snd_soc_card *card,
+	struct snd_soc_fw_card_ops *ops, const struct firmware *fw,
+	u32 index);
+int snd_soc_fw_load_platform(struct snd_soc_platform *platform,
+	struct snd_soc_fw_platform_ops *ops, const struct firmware *fw,
+	u32 index);
+int snd_soc_fw_load_codec(struct snd_soc_codec *codec,
+	struct snd_soc_fw_codec_ops *ops, const struct firmware *fw,
+	u32 index);
+
+#define SND_SOC_FW_INDEX_ALL	0	/* index that matches all FW objects */
+
+/* Firmware based dynamic widget and assoc kcontrol removal */
+void snd_soc_fw_dcontrols_remove_widgets(struct snd_soc_dapm_context *dapm,
+	u32 index);
+void snd_soc_fw_dcontrols_remove_widget(struct snd_soc_dapm_widget *w);
+
+/* Firmware based dynamic kcontrol removal for components */
+void snd_soc_fw_dcontrols_remove_codec(struct snd_soc_codec *codec, u32 index);
+void snd_soc_fw_dcontrols_remove_platform(struct snd_soc_platform *platform, u32 index);
+void snd_soc_fw_dcontrols_remove_card(struct snd_soc_card *soc_card, u32 index);
+int snd_soc_fw_dcontrols_remove_all(struct snd_soc_card *soc_card, u32 index);
+
+#endif