84b57bf557dbdadcef2fcf1f5e464e0ccf23ac59
Peter Ujfalusi <peter.ujfalusi@ti.com>
2014-08-14 15:11:29 +0300

    ASoC: tlv320aic3x: Add tlv320aic3104 as supported
    The driver can handle tlv310aic3104 just fine.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- sound/soc/codecs/tlv320aic3x.c
+++ sound/soc/codecs/tlv320aic3x.c
@@ -1456,6 +1456,7 @@ static const struct i2c_device_id aic3x_i2c_id[] = {
 	{ "tlv320aic3x", AIC3X_MODEL_3X },
 	{ "tlv320aic33", AIC3X_MODEL_33 },
 	{ "tlv320aic3007", AIC3X_MODEL_3007 },
+	{ "tlv320aic3104", AIC3X_MODEL_3X },
 	{ "tlv320aic3106", AIC3X_MODEL_3X },
 	{ }
 };
@@ -1601,6 +1602,7 @@ static const struct of_device_id tlv320aic3x_of_match[] = {
 	{ .compatible = "ti,tlv320aic3x", },
 	{ .compatible = "ti,tlv320aic33" },
 	{ .compatible = "ti,tlv320aic3007" },
+	{ .compatible = "ti,tlv320aic3104" },
 	{ .compatible = "ti,tlv320aic3106" },
 	{},
 };
