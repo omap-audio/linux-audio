b0d7cc439113abe56e1970edf2595071c56702eb
Peter Ujfalusi <peter.ujfalusi@ti.com>
2016-05-04 16:26:42 +0300

    ASoC: ti: davinci-mcasp: Move the clkdiv IDs to DT binding header
    Move the clkdiv defines to a new binding header file so they ca be used in
    DT files as well.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- sound/soc/ti/davinci-mcasp.h
+++ sound/soc/ti/davinci-mcasp.h
@@ -15,6 +15,8 @@
 #ifndef DAVINCI_MCASP_H
 #define DAVINCI_MCASP_H
 
+#include <dt-bindings/sound/ti-mcasp.h>
+
 /*
  * McASP register definitions
  */
@@ -294,14 +296,5 @@
 #define NUMEVT_MASK	(0xFF << 8)
 #define NUMEVT(x)	(((x) & 0xFF) << 8)
 #define NUMDMA_MASK	(0xFF)
-
-/* Source of High-frequency transmit/receive clock */
-#define MCASP_CLK_HCLK_AHCLK		0 /* AHCLKX/R */
-#define MCASP_CLK_HCLK_AUXCLK		1 /* Internal functional clock */
-
-/* clock divider IDs */
-#define MCASP_CLKDIV_AUXCLK		0 /* HCLK divider from AUXCLK */
-#define MCASP_CLKDIV_BCLK		1 /* BCLK divider from HCLK */
-#define MCASP_CLKDIV_BCLK_FS_RATIO	2 /* to set BCLK FS ration */
 
 #endif	/* DAVINCI_MCASP_H */
