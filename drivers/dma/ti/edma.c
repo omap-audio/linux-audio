cce2fa690e0e614fcbe2c602ca26e4e2d85a1f31
Peter Ujfalusi <peter.ujfalusi@ti.com>
2018-03-27 17:45:58 +0300

    dmaengine: ti: edma: Convert to SPDX licensing
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- drivers/dma/ti/edma.c
+++ drivers/dma/ti/edma.c
@@ -1,18 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * TI EDMA DMA engine driver
  *
- * Copyright 2012 Texas Instruments
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation version 2.
- *
- * This program is distributed "as is" WITHOUT ANY WARRANTY of any
- * kind, whether express or implied; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Copyright (C) 2012 Texas Instruments Incorporated - http://www.ti.com
  */
-
 #include <linux/dmaengine.h>
 #include <linux/dma-mapping.h>
 #include <linux/edma.h>
