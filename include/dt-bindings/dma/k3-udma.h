e280a9a01251ef07eb29005cc386e00477e026b8
Peter Ujfalusi <peter.ujfalusi@ti.com>
2018-09-24 13:28:10 +0300

    dt-bindings: dma: ti: Add document for K3 UDMA
    New binding document for
    Texas Instruments K3 NAVSS Unified DMA – Peripheral Root Complex (UDMA-P).
    
    UDMA-P is introduced as part of the K3 architecture and can be found on
    AM654 and j721e.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

---
+++ include/dt-bindings/dma/k3-udma.h
@@ -0,0 +1,10 @@
+#ifndef __DT_TI_UDMA_H
+#define __DT_TI_UDMA_H
+
+#define UDMA_DIR_TX		0
+#define UDMA_DIR_RX		1
+
+#define PSIL_STATIC_TR_NONE	0
+#define PSIL_STATIC_TR_XY	1
+
+#endif /* __DT_TI_UDMA_H */
