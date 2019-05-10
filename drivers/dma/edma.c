37fce4ba2c5949933b8971ae27a7064ed7074898
Peter Ujfalusi <peter.ujfalusi@ti.com>
2019-05-10 13:09:42 +0300

    dmaengine: ti: edma: Use bitmap_set() instead of open coded set_bits()
    bitmap_set() is the standard way of setting an area in the bitfield.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- drivers/dma/edma.c
+++ drivers/dma/edma.c
@@ -15,6 +15,7 @@
 
 #include <linux/dmaengine.h>
 #include <linux/dma-mapping.h>
+#include <linux/bitmap.h>
 #include <linux/edma.h>
 #include <linux/err.h>
 #include <linux/init.h>
@@ -400,12 +401,6 @@ static inline void edma_param_or(struct edma_cc *ecc, int offset, int param_no,
 				 unsigned or)
 {
 	edma_or(ecc, EDMA_PARM + offset + (param_no << 5), or);
-}
-
-static inline void set_bits(int offset, int len, unsigned long *p)
-{
-	for (; len > 0; len--)
-		set_bit(offset + (len - 1), p);
 }
 
 static inline void clear_bits(int offset, int len, unsigned long *p)
@@ -2176,7 +2171,7 @@ static int edma_probe(struct platform_device *pdev)
 {
 	struct edma_soc_info	*info = pdev->dev.platform_data;
 	s8			(*queue_priority_mapping)[2];
-	int			i, off, ln;
+	int			i, off;
 	const s16		(*rsv_slots)[2];
 	const s16		(*xbar_chans)[2];
 	int			irq;
@@ -2266,11 +2261,9 @@ static int edma_probe(struct platform_device *pdev)
 		/* Set the reserved slots in inuse list */
 		rsv_slots = info->rsv->rsv_slots;
 		if (rsv_slots) {
-			for (i = 0; rsv_slots[i][0] != -1; i++) {
-				off = rsv_slots[i][0];
-				ln = rsv_slots[i][1];
-				set_bits(off, ln, ecc->slot_inuse);
-			}
+			for (i = 0; rsv_slots[i][0] != -1; i++)
+				bitmap_set(ecc->slot_inuse, rsv_slots[i][0],
+					   rsv_slots[i][1]);
 		}
 	}
 
